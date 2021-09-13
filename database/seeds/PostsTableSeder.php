16bbfd1efcd07c3a0cb4aa6d9cc78f136138495f
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-09-13 16:02:46 +0200

    trying to create auth with laravel

---
+++ database/seeds/PostsTableSeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
+class PostsTableSeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //
+        for ($i = 0; $i < 50; $i++){
+           $postObject = new Post();
+           $postObject -> title = $faker->sentence(5);
+           $postObject-> content =$faker->text();
+           $postObject->author=$faker->text(50);
+           $postObject-> save(); 
+        }
+    }
+}
